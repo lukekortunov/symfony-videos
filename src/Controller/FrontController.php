7e9dd4863ece40bcf5da35de8f8617053cb29ea6
Luke Kortunov <ikortunov@crispwp.com>
2019-07-23 17:31:20 +0300

    Lesson #119

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
@@ -34,6 +35,50 @@ class FrontController extends AbstractController
     public function videoDetails()
     {
         return $this->render('front/video_details.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/search-results", name="search_results", methods={"POST"})
+     */
+    public function searchResults()
+    {
+        
+        return $this->render( 'front/search_results.html.twig' );
+        
+    }
+    
+    
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        
+        return $this->render('front/pricing.html.twig');
+        
+    }
+    
+    
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        
+        return $this->render('front/register.html.twig');
+        
+    }
+    
+    
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        
+        return $this->render('front/login.html.twig');
+        
     }
     
 }
