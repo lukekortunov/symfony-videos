1651c2df83f73a6da1a9263c6e880d140bcd19f1
Luke Kortunov <ikortunov@crispwp.com>
2019-07-23 16:48:18 +0300

    Lesson #118

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -18,4 +18,22 @@ class FrontController extends AbstractController
         ]);
     }
     
+    
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/videolist.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+    
 }
